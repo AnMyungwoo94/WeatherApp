f14b873c3c8a66045183e84b920a7ed7d9df6070
AnMyungwoo94 <anmy121212@gmail.com>
2023-11-16 15:44:41 +0900

    [update]날씨 위젯기능 추가 중

---
+++ app/src/main/java/com/myungwoo/weatherapp/widget/WeatherAppWidgetProvider.kt
@@ -0,0 +1,41 @@
+package com.myungwoo.weatherapp.widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.myungwoo.weatherapp.MainActivity
+import com.myungwoo.weatherapp.R
+
+class WeatherAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity
+            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+                .let { intent -> //FLAG_IMMUTABLE 안드로이드 12부터는 선언을 해줘야해
+                    PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+                }
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.widget_weather
+            ).apply {
+                setOnClickPendingIntent(R.id.testTextView, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+}
