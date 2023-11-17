da482bada645a5b5f800d6df9b6918c6857357d8
AnMyungwoo94 <anmy121212@gmail.com>
2023-11-17 22:49:23 +0900

    [update] 위젯설정

--- app/src/main/java/com/myungwoo/weatherapp/MainActivity.kt
+++ app/src/main/java/com/myungwoo/weatherapp/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.myungwoo.weatherapp
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.RemoteViews
 import android.widget.TextView
 import com.myungwoo.weatherapp.databinding.ActivityMainBinding
 import com.myungwoo.weatherapp.network.WeatherRepository
 import com.myungwoo.weatherapp.network.data.WeatherData
 import com.myungwoo.weatherapp.network.data.WeekendWeatherData
+import com.myungwoo.weatherapp.widget.WeatherAppWidgetProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -39,9 +45,16 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.adapter = recyclerViewAadapter
 
         //스피너 만들기
-        val spinnerList = resources.getStringArray(R.array.area) // res/values/arrays.xml에 정의된 "area" 배열에서 지역명을 가져와서 리스트로 만든다.
-        val spinnerItems = spinnerList.map { SpinnerData(it) } // 각 지역명을 SpinnerData 객체로 매핑하여 리스트를 생성한다.
-        val adapter = ArrayAdapter(this, R.layout.spinner_item, R.id.textView, spinnerList) // ArrayAdapter를 생성하여 스피너에 사용할 데이터와 레이아웃을 설정한다.
+        val spinnerList =
+            resources.getStringArray(R.array.area) // res/values/arrays.xml에 정의된 "area" 배열에서 지역명을 가져와서 리스트로 만든다.
+        val spinnerItems =
+            spinnerList.map { SpinnerData(it) } // 각 지역명을 SpinnerData 객체로 매핑하여 리스트를 생성한다.
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.spinner_item,
+            R.id.textView,
+            spinnerList
+        ) // ArrayAdapter를 생성하여 스피너에 사용할 데이터와 레이아웃을 설정한다.
         binding.searchSpinner.adapter = adapter // 생성한 어댑터를 스피너에 연결합니다.
         binding.searchSpinner.setSelection(0) // 스피너 처음값 지정
 
@@ -64,6 +77,7 @@ class MainActivity : AppCompatActivity() {
                     try {
                         //선택된 selectedItemId 값으로 API 불러오기 즉, 지역별 ID값으로 불러오는것.
                         val weatherCurrentList = netWorkRepository.getCurrentList(selectedItemId)
+//                        Log.e("날씨 데이터", weatherCurrentList.toString())
                         val forecastWeatherData = netWorkRepository.getWeekendList(selectedItemId)
 
                         //리사이클러뷰에 뿌려줄 Data를 weekendData 넣어주기
@@ -73,12 +87,16 @@ class MainActivity : AppCompatActivity() {
                         //스피너 클릭시 지역별로 최근날씨 변경해주기
                         updateCurrentList(weatherCurrentList)
 
+                        // 위젯 만들기 위해서 intent로 전달해줌
+                        createWidget(weatherCurrentList)
+
                     } catch (e: Exception) {
                         // Handle network errors
                         Log.e("NetworkError", e.message ?: "Unknown error")
                     }
                 }
             }
+
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 // Handle case where nothing is selected
                 TODO("Not yet implemented")
@@ -114,4 +132,30 @@ class MainActivity : AppCompatActivity() {
         return String.format("%.1f ℃", kelvinTemp - 273.15)
     }
 
+    private fun getFormattedDate(timestamp: Long): String {
+        val date = Date(timestamp * 1000)
+        val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return format.format(date)
+    }
+
+    private fun getFormattedTime(timestamp: Long): String {
+        val date = Date(timestamp * 1000)
+        val format = SimpleDateFormat("a hh:mm", Locale.getDefault())
+        return format.format(date)
+    }
+
+    private fun createWidget(weatherCurrentList: WeatherData) {
+        val appWidgetManager = AppWidgetManager.getInstance(this)
+        val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(this, WeatherAppWidgetProvider::class.java))
+
+        appWidgetIds.forEach { appWidgetId ->
+            val views = RemoteViews(packageName, R.layout.widget_weather)
+            views.setTextViewText(R.id.tempTextView, convertFormatTemperature(weatherCurrentList.main.temp))
+            views.setTextViewText(R.id.dayTextView, getFormattedDate(weatherCurrentList.dt))
+            views.setTextViewText(R.id.timeTextView, getFormattedTime(weatherCurrentList.dt))
+
+            // 위젯 업데이트
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
 }
--- app/src/main/java/com/myungwoo/weatherapp/network/data/WeatherData.kt
+++ app/src/main/java/com/myungwoo/weatherapp/network/data/WeatherData.kt
@@ -1,5 +1,9 @@
 package com.myungwoo.weatherapp.network.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class WeatherData(
     val coord: Coord,
     val weather: List<Weather>,
@@ -15,20 +19,21 @@ data class WeatherData(
     val id: Int,
     val name: String,
     val cod: Int
-)
+) : Parcelable
 
+@Parcelize
 data class Coord(
     val lon: Double,
     val lat: Double
-)
-
+) : Parcelable
+@Parcelize
 data class Weather(
     val id: Int,
     val main: String,
     val description: String,
     val icon: String
-)
-
+): Parcelable
+@Parcelize
 data class Main(
     val temp: Double,
     val feels_like: Double,
@@ -38,26 +43,26 @@ data class Main(
     val humidity: Int,
     val sea_level: Int,
     val grnd_level: Int
-)
-
+): Parcelable
+@Parcelize
 data class Wind(
     val speed: Double,
     val deg: Int,
     val gust: Double
-)
-
+): Parcelable
+@Parcelize
 data class Rain(
     val hour : Double
-)
-
+): Parcelable
+@Parcelize
 data class Clouds(
     val all: Int
-)
-
+): Parcelable
+@Parcelize
 data class Sys(
     val type: Int,
     val id: Int,
     val country: String,
     val sunrise: Long,
     val sunset: Long
-)
+): Parcelable
